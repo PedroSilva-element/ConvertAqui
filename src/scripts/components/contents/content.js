6f256ccdf9694d18e5d3dc497a6a9440ee5e9406
Pedro Silva <pedrovenancioferreira@gmail.com>
2022-05-31 15:03:21 -0300

    ConvertAqui -Version 2.0

---
+++ src/scripts/components/contents/content.js
@@ -0,0 +1,18 @@
+
+export const article01 = () => {
+    return `
+    <h1>Grandeza Física</h1>
+    <p>Grandeza física é tudo aquilo que envolve medidas, ou seja, que pode ser medida.
+    Medir significa comparar quantitativamente uma grandeza física com uma unidade através de
+    uma escala pré-definida. Em outras palavras, medir uma grandeza física é compará-la com
+    outra grandeza de mesma espécie, que é a unidade de medida. Verifica-se, então, quantas
+    vezes a unidade está contida na grandeza que está sendo medida. Nas medições, as
+    grandezas sempre devem vir acompanhadas de unidades.
+    Por exemplo, o comprimento de uma corda pode ser medida em metros. Quando se
+    diz que um determinado pedaço de corda tem 3 m de comprimento, significa dizer que esta
+    corda pode ser dividida em 3 pedaços de 1 metro, onde 1 metro é a unidade. Por outro lado,
+    este mesmo pedaço de corda pode ser dividido em 300 pedaços de 1 centímetro, onde 1
+    centímetro também é uma unidade. Em ambos os casos, a grandeza física é a mesma: “comprimento da corda”, embora as unidades sejam distintas. </p>
+    <p>Outros exemplos de grandezas:<br> 
+    <strong>físicas: massa, temperatura, velocidade, etc.</strong></p>`
+};
--- src/scripts/converter.js
+++ src/scripts/converter.js
@@ -3,15 +3,16 @@ import { listViewmgt } from "./components/module-seach/toView.js";
 import { actionCalc } from "./components/calc.js";
 import { itensBusca } from "./components/itensBusca/itensSeach.js";
 import { reset } from "./components/reset.js";
-//import values from "./database/values.js";
-//import rules from "./database/rules.js";
+import { article01 } from "./components/contents/content.js";
 
 const seachUnits = document.querySelector('.trigger');
 const botCalc = document.querySelector(".calc");
 const redo = document.querySelector(".reset")
+const contentArea = document.querySelector(".content-area")
 
 window.onload = function () {
         listViewmgt();
+        contentArea.innerHTML = article01();    //Content Theoretical
 }
 
 //Seach!!
